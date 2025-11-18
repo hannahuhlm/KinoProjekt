2434379b4a6f2f087bf07ca85a550d5b5835f98e
admin <admin@NB23-762.ha.bib.de>
2025-11-18 13:14:57 +0100

    Die Oberfläche klappt jetzt

--- src/main/java/kino/application/MainView.java
+++ src/main/java/kino/application/MainView.java
@@ -1,19 +1,43 @@
 package kino.application;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.menubar.MenuBar;
+import kino.application.*;
 
 @Route("")
 public class MainView extends VerticalLayout {
     /**
 	 * 
+	 * 				
 	 */
 	private static final long serialVersionUID = 1L;
 
 	public MainView() {
-        Button button = new Button();
-        button.addClickListener(e -> button.setText("Clicked!"));
-        add(button);
+		H1 text = new H1("Kinotastisch");
+		add(text);
+		H2 whatsNext = new H2("Was möchten Sie tun?");
+		add(whatsNext);
+		tabsBasic();
     }
+	
+	public void tabsBasic() {
+		Tab details = new Tab("Kinosaal anlegen");
+		Tab payment = new Tab("Filmzuordnung");
+		Tab shipping = new Tab("Sitzplatz reservieren");
+		Tab booking= new Tab("Sitzplatz buchen");
+		Tabs tabs = new Tabs(details, payment, shipping, booking);
+		add(tabs);
+	}
 }
