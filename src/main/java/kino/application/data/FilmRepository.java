6d191997e415b94b711e1a1cb2fb88a45a6ad60d
Jasototeles <jason.luck@edu.fhdw.de>
2025-11-21 17:16:37 +0100

    FilmRepository hinzugefügt

---
+++ src/main/java/kino/application/data/FilmRepository.java
@@ -0,0 +1,23 @@
+package kino.application.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository für die Film-Entity.
+ *
+ * - Film ist eine @Entity-Klasse -> entspricht einer Tabelle in der Datenbank.
+ * - JpaRepository<Film, Long> bedeutet:
+ *      * Film  = auf welche Tabelle/Entity sich das Repository bezieht
+ *      * Long  = Typ des Primärschlüssels (id vom Film)
+ *
+ * Über dieses Interface bekommen wir automatisch Methoden wie:
+ *  - findAll()     -> SELECT * FROM film
+ *  - findById(id)  -> SELECT * FROM film WHERE id = ?
+ *  - save(film)    -> INSERT / UPDATE film
+ *  - delete(film)  -> DELETE FROM film ...
+ */
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Long> {
+
+}
