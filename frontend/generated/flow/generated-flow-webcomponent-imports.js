c90b4cda1a3dd7056d4e3292f5afb7484617a500
Jasototeles <jason.luck@edu.fhdw.de>
2025-11-24 13:19:38 +0100

    Schönere Startseite

--- frontend/generated/flow/generated-flow-webcomponent-imports.js
+++ frontend/generated/flow/generated-flow-webcomponent-imports.js
@@ -5,6 +5,7 @@ import { injectGlobalCss } from 'Frontend/generated/jar-resources/theme-util.js'
 import { css, unsafeCSS, registerStyles } from '@vaadin/vaadin-themable-mixin';
 import $cssFromFile_0 from 'Frontend/styles/film-list.css?inline';
 import $cssFromFile_1 from 'Frontend/styles/kontakt.css?inline';
+import $cssFromFile_2 from 'Frontend/styles/startseite.css?inline';
 import '@vaadin/field-highlighter/theme/lumo/vaadin-field-highlighter.js';
 import '@vaadin/common-frontend/ConnectionIndicator.js';
 import '@vaadin/polymer-legacy-adapter/style-modules.js';
@@ -99,6 +100,9 @@ injectGlobalWebcomponentCss($cssFromFile_0.toString());
 
 injectGlobalCss($cssFromFile_1.toString(), 'CSSImport end', document);
 injectGlobalWebcomponentCss($cssFromFile_1.toString());
+
+injectGlobalCss($cssFromFile_2.toString(), 'CSSImport end', document);
+injectGlobalWebcomponentCss($cssFromFile_2.toString());
 const loadOnDemand = (key) => { return Promise.resolve(0); }
 window.Vaadin = window.Vaadin || {};
 window.Vaadin.Flow = window.Vaadin.Flow || {};
